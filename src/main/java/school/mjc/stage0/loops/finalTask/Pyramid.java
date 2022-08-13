4a193d3a2079f0e771bf7ef7e10a81ae7181e025
nkere30 <ninokereselidze30@gmail.com>
2022-08-13 22:07:28 +0400

    Completed

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -4,7 +4,7 @@ public class Pyramid {
     public void printPyramid(int cathetusLength) {
         for(int i = 1; i <= cathetusLength; i++) {
             for(int k = cathetusLength; k > i; k--) {
-                System.out.print("-");
+                System.out.print(" ");
             }
             for(int j = 0; j < i; j++) {
                 System.out.print(i - j);
