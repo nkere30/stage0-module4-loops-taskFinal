29f450f6bd756c28c9483687dcac7c3517887ab1
nkere30 <ninokereselidze30@gmail.com>
2022-08-13 22:02:38 +0400

    Completed

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for(int i = 1; i <= cathetusLength; i++) {
+            for(int k = cathetusLength; k > i; k--) {
+                System.out.print("-");
+            }
+            for(int j = 0; j < i; j++) {
+                System.out.print(i - j);
+            }
+            for(int j = 2; j <= i; j++) {
+                        System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(4);
     }
 }
